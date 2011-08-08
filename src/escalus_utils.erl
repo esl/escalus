aa578f9ae570a34b269a0dd3a1bd171f014d7e08
Krzysztof Goj <krzysztof.goj@erlang-solutions.com>
2011-08-08 15:03:28 +0200

    Bugfix: use bare jids when making friends

--- src/escalus_utils.erl
+++ src/escalus_utils.erl
@@ -16,7 +16,11 @@
 
 -module(escalus_utils).
 
--export([log_stanzas/2, exchange_stanzas/2]).
+-export([log_stanzas/2,
+         exchange_stanzas/3,
+         distinct_ordered_pairs/2,
+         each_with_index/3
+        ]).
 
 -include("include/escalus.hrl").
 -include_lib("exmpp/include/exmpp.hrl").
@@ -26,13 +30,17 @@ log_stanzas(Comment, Stanzas) ->
     StanzaLines = [["\n  * ", exmpp_xml:document_to_iolist(S)] || S <- Stanzas],
     error_logger:info_msg("~s:~s~n", [Comment, StanzaLines]).
 
--spec exchange_stanzas([#client{}], #xmlel{}) -> any().
-exchange_stanzas(Clients, BaseStanza) ->
+-spec exchange_stanzas([#client{}], #xmlel{}, full_jid|short_jid) -> any().
+exchange_stanzas(Clients, BaseStanza, JidType) ->
     % send BaseStanza to everyone
-    all_to_all(fun(Sender, #client{jid=RecipientJid}) ->
-        Stanza = exmpp_stanza:set_recipient(BaseStanza, RecipientJid),
+    distinct_ordered_pairs(fun(Sender, #client{jid=RecipientJid}) ->
+        Jid = case JidType of
+            full_jid -> RecipientJid;
+            short_jid -> make_short_jid(RecipientJid)
+        end,
+        Stanza = exmpp_stanza:set_recipient(BaseStanza, Jid),
         escalus_client:send(Sender, Stanza)
-    end, Clients, Clients),
+    end, Clients),
 
     % wait for everyone to receive it
     StanzaCount = length(Clients) - 1, % get BaseStanza from everyone else, but me (-1)
@@ -40,18 +48,30 @@ exchange_stanzas(Clients, BaseStanza) ->
         escalus_client:wait_for_stanzas(Client, StanzaCount)
     end, Clients).
 
-%% As and Bs are lists of equal length
-%% calls Fun(A, B) on all but corresponding elements
-%% of that lists
-all_to_all(Fun, As, Bs) ->
-    lists:foldl(fun(A, N) ->
-        lists:foldl(fun(B, M) ->
+%% calls Fun(A, B) on each distinct (A =/= B) ordered pair of elements in List
+distinct_ordered_pairs(Fun, List) ->
+    K = each_with_index(fun(A, N) ->
+        each_with_index(fun(B, M) ->
             if N =/= M ->
                     Fun(A, B);
                true ->
                    skip
-           end,
-           M + 1
-        end, 1, Bs),
+           end
+        end, 0, List)
+    end, 0, List),
+    K * (K - 1).
+
+%% Calls Fun(Element, Index) for indices (starting from Start) and elements of List
+each_with_index(Fun, Start, List) ->
+    lists:foldl(fun(Element, N) ->
+        Fun(Element, N),
         N + 1
-    end, 1, As).
+    end, Start, List).
+
+%%--------------------------------------------------------------------
+%% Helpers
+%%--------------------------------------------------------------------
+make_short_jid(Jid) ->
+    {match, [ShortJid]} =
+        re:run(Jid, <<"^([^/]*)">>, [{capture, all_but_first, binary}]),
+    ShortJid.
