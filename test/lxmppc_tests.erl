6e64c0a3485b7b51d0c8cec670540cd56bef29c9
Krzysztof Goj <krzysztof.goj@gmail.com>
2012-02-24 13:32:18 +0100

    Removed ugly test
    It followed "test by copy-paste" mis-pattern

--- test/lxmppc_tests.erl
+++ test/lxmppc_tests.erl
@@ -12,16 +12,3 @@
 -include("lxmppc.hrl").
 
 -compile(export_all).
-
-auth_plain_stanzas_test() ->
-    Username = <<"romeo">>,
-    Password = <<"I <3 Juliet">>,
-    Stanza = lxmppc_stanza:auth_plain(Username, Password),
-    ExpectedAttrs = [{<<"xmlns">>, <<"urn:ietf:params:xml:ns:xmpp-sasl">>},
-                     {<<"mechanism">>, <<"PLAIN">>}],
-    ?assertEqual(<<"auth">>, Stanza#xmlelement.name),
-    ?assertEqual(ExpectedAttrs, Stanza#xmlelement.attrs),
-    [CData] = Stanza#xmlelement.body,
-    Payload = base64:decode(exml:unescape_cdata(CData)),
-    ExpectedAuth = <<0:8, Username/binary, 0:8, Password/binary>>,
-    ?assertEqual(ExpectedAuth, Payload).
