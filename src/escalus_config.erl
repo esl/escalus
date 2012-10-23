4fe8e6135a82e94506b1e204fdd2a159d847d760
Jan Daniel Bothma <jan.bothma@erlang-solutions.com>
2012-10-23 10:19:21 +0200

    Add ct config traversal shortcut

--- src/escalus_config.erl
+++ src/escalus_config.erl
@@ -20,7 +20,8 @@
 -export([get_config/2,
          get_config/3,
          get_config/4,
-         get_config/5]).
+         get_config/5,
+         get_ct/1]).
 
 %%--------------------------------------------------------------------
 %% Public API
@@ -52,3 +53,21 @@ get_config(USName, UserSpec, CName, Config, Default) ->
         false ->
             get_config(CName, Config, Default)
     end.
+
+
+get_ct(Required) when is_atom(Required) ->
+    ct:get_config(Required);
+get_ct(Required) when is_tuple(Required) ->
+    TopList = ct:get_config(erlang:element(1, Required)),
+    get_ct_recurse(Required, 2, TopList).
+
+
+get_ct_recurse(Required, Pos, LevelVal) when Pos < size(Required) ->
+    Key = erlang:element(Pos, Required),
+    {_, NewLevelVal} = lists:keyfind(Key, 1, LevelVal),
+    get_ct_recurse(Required, Pos+1, NewLevelVal);
+
+get_ct_recurse(Required, Pos, LevelVal) when Pos == size(Required) ->
+    Key = erlang:element(Pos, Required),
+    {_, NewLevelVal} = lists:keyfind(Key, 1, LevelVal),
+    NewLevelVal.
