ed23eee96fa204d806231cd1dcead9f22dc14e3e
Simon Zelazny <zelazny@mailbox.org>
2016-03-11 09:38:53 +0100

    Clean up duplicated code

--- src/escalus_fresh.erl
+++ src/escalus_fresh.erl
@@ -1,20 +1,26 @@
 -module(escalus_fresh).
 -export([story/3, story_with_config/3]).
 
+% TODO unify this type across escalus
+-type userspec() :: {atom(), integer()}.
+-type config() :: escalus:config().
+
+-spec story_with_config(config(), [userspec()], fun()) -> any().
 story_with_config(Config, UserSpecs, StoryFun) ->
-    FreshSpecs = fresh_specs(Config, UserSpecs, fresh_suffix()),
-    case length(FreshSpecs) == length(UserSpecs) of
-        false -> ct:fail("failed to get required users"); _ -> ok end,
-    FreshConfig = escalus_users:create_users(Config, FreshSpecs),
+    {FreshConfig, FreshSpecs} = create_fresh_users(Config, UserSpecs),
     escalus:story(FreshConfig, UserSpecs,
                   fun(Args) -> apply(StoryFun, [FreshConfig|Args]) end).
 
 story(Config, UserSpecs, StoryFun) ->
+    {FreshConfig, FreshSpecs} = create_fresh_users(Config, UserSpecs),
+    escalus:story(FreshConfig, UserSpecs, StoryFun).
+
+create_fresh_users(Config, UserSpecs) ->
     FreshSpecs = fresh_specs(Config, UserSpecs, fresh_suffix()),
     case length(FreshSpecs) == length(UserSpecs) of
         false -> ct:fail("failed to get required users"); _ -> ok end,
     FreshConfig = escalus_users:create_users(Config, FreshSpecs),
-    escalus:story(FreshConfig, UserSpecs, StoryFun).
+    {FreshConfig, FreshSpecs}.
 
 fresh_specs(Config, TestedUsers, StorySuffix) ->
     AllSpecs = escalus_config:get_config(escalus_users, Config),
@@ -27,12 +33,12 @@ make_fresh_username({N, UserConfig}, Suffix) ->
     {N, lists:keyreplace(username, 1, UserConfig, {username, NewName})}.
 
 select(UserResources, FullSpecs) ->
-    Fst = fun({A,_}) -> A end,
+    Fst = fun({A, _}) -> A end,
     UserNames = lists:map(Fst, UserResources),
     lists:filter(fun({Name, _}) -> lists:member(Name, UserNames) end,
                  FullSpecs).
 
 fresh_suffix() ->
-    {_,S,US} = erlang:now(),
+    {_, S, US} = erlang:now(),
     L = lists:flatten([integer_to_list(S rem 100), ".", integer_to_list(US)]),
     list_to_binary(L).
